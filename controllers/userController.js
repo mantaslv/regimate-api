8bd85b4e2aa17f41d86e99c364eee449e1614092
mantaslv <mantasvolkas@gmail.com>
2023-03-20 13:51:21 +0000

    Added jwt token to signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.JWT_SECRET, { expiresIn: '2h'});
+};
 
 const loginUser = async (req, res) => {
     res.json({msg: 'login user'});
@@ -8,7 +13,10 @@ const signupUser = async (req, res) => {
     const {email, password} = req.body;
     try {
         const user = await User.signup(email, password);
-        res.status(201).json({email, user});
+        const token = createToken(user._id);
+        const _id = user._id
+
+        res.status(201).json({_id, email, token});
     } catch (error) {
         res.status(400).json({error: error.message});
     };
