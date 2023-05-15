3dc7850bc63ee2564dce10e07e1c4eddf14ca0f8
mantaslv <mantasvolkas@gmail.com>
2023-05-15 17:32:26 +0100

    Added workout route to server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const exerciseRoutes = require('./routes/exercises');
+const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
 
 require('dotenv').config();
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/exercises', exerciseRoutes);
+app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
 
 if (process.env.NODE_ENV !== "test") {
