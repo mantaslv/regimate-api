f798182fb8fbef1df91b9c498035ebd8e29696f4
mantaslv <mantasvolkas@gmail.com>
2023-05-15 17:19:25 +0100

    Workout Model Schema completed

---
+++ models/workoutModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const setSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    weight: String,
+    reps: String
+});
+
+const exerciseSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    exerciseName: {
+        type: String,
+        required: true
+    },
+    sets: [setSchema]
+});
+
+const workoutSchema = new Schema({
+    user_id: {
+        type: String,
+        required: true,
+    },
+    exercises: [exerciseSchema]
+}, { timestamps: true });
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
