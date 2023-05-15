417609619c32d2d300b6856a44d86e4115b59944
mantaslv <mantasvolkas@gmail.com>
2023-05-15 18:15:39 +0100

    Added auth to workouts route

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const requireAuth = require('../middleware/requireAuth');
 const { createWorkout } = require('../controllers/workoutController');
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.post('/', createWorkout);
 
