8127e2394baa4e1095d9f8e8cfe20491ec30bd25
mantaslv <mantasvolkas@gmail.com>
2023-05-16 10:46:10 +0100

    Added getWorkouts controller to route

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const requireAuth = require('../middleware/requireAuth');
-const { createWorkout } = require('../controllers/workoutController');
+const { createWorkout, getWorkouts } = require('../controllers/workoutController');
 
 const router = express.Router();
 
 router.use(requireAuth);
 
 router.post('/', createWorkout);
+router.get('/', getWorkouts);
 
 module.exports = router;
