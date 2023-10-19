3e9ee2180c97c9d3c7ff642edb78148679da2d53
mantaslv <mantasvolkas@gmail.com>
2023-10-19 13:52:33 +0100

    Removed auth requirement for exercise directory

--- routes/exerciseDirectory.js
+++ routes/exerciseDirectory.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const requireAuth = require('../middleware/requireAuth');
 const getExercises = require('../controllers/exerciseDirectoryController');
 
 const router = express.Router();
-
-router.use(requireAuth);
 
 router.get('/', getExercises);
 
