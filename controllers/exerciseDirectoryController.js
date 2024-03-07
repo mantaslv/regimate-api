2c20924c93e9c2b06e7475ec0159c8e60c29a633
mantaslv <mantasvolkas@gmail.com>
2024-03-07 12:10:48 +0000

    Converted exercise directory output to exerciseName instead of name

--- controllers/exerciseDirectoryController.js
+++ controllers/exerciseDirectoryController.js
@@ -2,9 +2,17 @@ const ExerciseDirectoryModel = require('../models/exerciseDirectoryModel');
 const mongoose = require('mongoose');
 
 const getExercises = async (req, res) => {
-    const exercises = await ExerciseDirectoryModel.find({});
+    try {
+        const exercises = await ExerciseDirectoryModel.find({});
+        const renamedExercises = exercises.map(exercise => {
+            const { name, ...otherProps } = exercise.toObject();
+            return { exerciseName: name, ...otherProps };
+        });
 
-    res.status(200).json(exercises);
+        res.status(200).json(renamedExercises);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports = getExercises;
